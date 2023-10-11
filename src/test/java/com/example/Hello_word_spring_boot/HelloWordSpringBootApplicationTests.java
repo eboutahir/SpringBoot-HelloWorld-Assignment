c568976e3760d7897a3134f45be232b2888d0689
manalechehbi <manale.chehbii@gmail.com>
2023-10-11 23:08:15 +0100

    add my  assignment in git

---
+++ src/test/java/com/example/Hello_word_spring_boot/HelloWordSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hello_word_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
