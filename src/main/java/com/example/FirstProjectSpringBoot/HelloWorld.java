97574ffb1cd902f5746d1f6c87ba9a2dd87cc849
YasminaDada <yasminadada18@gmail.com>
2023-10-11 21:24:51 +0100

    First Commit

---
+++ src/main/java/com/example/FirstProjectSpringBoot/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.FirstProjectSpringBoot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "Hello world!";
+    }
+}
