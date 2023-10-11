b37f0e63cab07bbc4c6e1a26bf995c97e730bcd8
khalidhaddou <khalidhaddou123456@gmail.com>
2023-10-11 22:21:51 +0100

    Ajout de projet de hello wolrd

---
+++ src/main/java/HelloWorldApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
