149127a83842df1a07a6f7064c01615a0d6f3e03
BRAHIM ELJANDOUZ <brahimeljandouz200@gmail.com>
2023-10-12 15:43:29 +0100

    hello-w

---
+++ src/main/java/com/javaguides/hello/hellocontroller.java
@@ -0,0 +1,12 @@
+package com.javaguides.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellocontroller {
+@GetMapping("/hello")
+    public String hello(){
+        return "hello word";
+    }
+}
