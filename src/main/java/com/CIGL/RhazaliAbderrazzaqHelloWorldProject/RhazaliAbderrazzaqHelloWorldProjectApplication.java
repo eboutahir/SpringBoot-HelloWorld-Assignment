c1d4385b6646b6b311146b16c8db94e43620fef2
Your Name <rhazali@gmial.com>
2023-10-12 17:28:32 +0100

    ok

---
+++ src/main/java/com/CIGL/RhazaliAbderrazzaqHelloWorldProject/RhazaliAbderrazzaqHelloWorldProjectApplication.java
@@ -0,0 +1,14 @@
+package com.CIGL.RhazaliAbderrazzaqHelloWorldProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhazaliAbderrazzaqHelloWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhazaliAbderrazzaqHelloWorldProjectApplication.class, args);
+		System.out.println("\nHello world !!!!!!!!");
+	}
+
+}
