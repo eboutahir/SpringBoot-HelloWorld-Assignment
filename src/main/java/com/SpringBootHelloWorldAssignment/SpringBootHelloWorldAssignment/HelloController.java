34853da20db55820e143fe0ccab0c36433ba09b1
BABAHAJ Zakarya <z.babahaj@edu.umi.ac.ma>
2023-10-12 18:48:34 +0100

    Hands-on work 1

--- src/main/java/com/SpringBootHelloWorldAssignment/SpringBootHelloWorldAssignment/HelloController.java
+++ src/main/java/com/SpringBootHelloWorldAssignment/SpringBootHelloWorldAssignment/HelloController.java
@@ -4,11 +4,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
-@RequestMapping("/sayhi")
+//@RequestMapping("/sayhi")
 public class HelloController {
-    @GetMapping
+    @GetMapping("/hello")
     public String sayHi(){
-        return "Hello world";
+        int hour = LocalTime.now().getHour();
+        if(hour > 12)
+            return "Good Morning !";
+        else
+            return "Hello !";
     }
+
+    @GetMapping("/hello-multiple")
+    public String helloMultiple(){
+        StringBuilder message = new StringBuilder();
+        for(int i=0; i<5; i++)
+            message.append("Hello, World !").append(i).append("<br>");
+        return message.toString();
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        int hour = LocalTime.now().getHour();
+        if(hour < 12)
+            return "Good Morning !";
+        else if (hour < 17)
+            return "Good afternoon !";
+        else
+            return "Good evening !";
+    }
+
 }
