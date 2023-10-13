d42c2ba9a874bbd78a2381aee0e8882b0c7db0fe
imane1 <bouassel.imane1@gmail.com>
2023-10-13 10:04:16 +0000

    first commit

--- src/main/java/com/ab/abdo/HelloWorldController.java
+++ src/main/java/com/ab/abdo/HelloWorldController.java
@@ -6,12 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @Controller
 public class HelloWorldController {
 
     @RequestMapping(value="/bouassel")
     public String helloWorld(Model model) {
-        model.addAttribute("message", "salam !");
-        return "hello.html";
+        int hour= LocalTime.now().getHour();
+        if(hour <=11){
+            model.addAttribute("message", "Good morning");
+            return "sun";
+        }
+        else if (hour <= 16){
+            model.addAttribute("message", "Good afternoon");
+            return "hello";
+        }
+        else {
+            model.addAttribute("message", "good evening");
+            return "hello";
+        }
+
     }
 }
