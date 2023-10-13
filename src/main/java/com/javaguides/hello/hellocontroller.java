d4f5c3b327631cbd58948276bda4785e9017eddf
BRAHIM ELJANDOUZ <brahimeljandouz200@gmail.com>
2023-10-13 20:41:55 +0100

    hello-w

--- src/main/java/com/javaguides/hello/hellocontroller.java
+++ src/main/java/com/javaguides/hello/hellocontroller.java
@@ -3,10 +3,20 @@ package com.javaguides.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
 public class hellocontroller {
 @GetMapping("/hello")
     public String hello(){
-        return "hello word";
+        int h= LocalTime.now().getHour();
+        if(h>=0 && h<12){
+            return "Good Morning";
+        }
+        else if(h>=12 && h<17){
+            return "Good Afternoon";
+        }
+        else
+            return "Good evening";
     }
 }
