e7b338e3d7ef6ae5ffc8c5350a842ac8291ea685
manalechehbi <manale.chehbii@gmail.com>
2023-10-15 23:38:58 +0100

    my second assignment

--- src/main/java/com/example/Hello_word_spring_boot/HelloWordSpringBootApplication.java
+++ src/main/java/com/example/Hello_word_spring_boot/HelloWordSpringBootApplication.java
@@ -2,12 +2,15 @@ package com.example.Hello_word_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloWordSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWordSpringBootApplication.class, args);
+
 	}
+
 
 }
--- src/main/java/com/example/Hello_word_spring_boot/hello_word.java
+++ src/main/java/com/example/Hello_word_spring_boot/hello_word.java
@@ -2,12 +2,21 @@ package com.example.Hello_word_spring_boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.time.LocalTime;
 @RestController
 public class hello_word
 {
     @GetMapping("/hello")
     public String Hello(){
-        return("Hello, World!");
+                String salutation;
+
+                if ( LocalTime.now().isBefore(LocalTime.NOON)) {
+                    salutation = "Good morning";
+                } else if ( LocalTime.now().isBefore(LocalTime.of(17, 0))) {
+                    salutation = "Good afternoon";
+                } else {
+                    salutation = "Good evening";
+                }
+                return "Hello! I am manal ,  " + salutation;
     }
 }
