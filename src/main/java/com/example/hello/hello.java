101296d78cf353de5028d59132331f02f9c81676
ouma1a <elalaouioumaima5@gmail.com>
2023-10-16 00:10:57 +0000

    hello chalenge

--- src/main/java/com/example/hello/hello.java
+++ src/main/java/com/example/hello/hello.java
@@ -1,13 +1,35 @@
 package com.example.hello;
+import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
 public class hello {
     @GetMapping("/hello")
-    public String helloworld()
-    {
-        return "hello it's my first spring boot project";
+    public String helloworld(Model model) {
+        int hour = LocalTime.now().getHour();
+        String message;
+        if (hour >= 0 && hour < 12) {
+            message = "Good Moring!";
+
+        } else if (hour >= 12 && hour < 17) {
+            message = "good afternoon";
+        } else {
+            message = "Good evening";
+        }
+        // return "hello it's my first spring boot project";
+        model.addAttribute("m", message);
+        return "hellowo";
+    }
+    public String htmlcode(String m) {
+        String htmlmessage =
+                "<!Doctype html>" + "\n" +
+                        "<html>" + "\n" +
+                        " <h1>" + m + "</h1>";
+        return htmlmessage;
+
     }
 }
