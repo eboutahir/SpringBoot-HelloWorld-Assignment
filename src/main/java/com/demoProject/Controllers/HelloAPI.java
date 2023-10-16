f43e43552a2cfba0a3dc6ebbb2f81aacbee1d83c
Ossamadeve <o.aabil@edu.umi.ac.ma>
2023-10-16 01:35:31 +0100

    hello world!

--- src/main/java/com/demoProject/Controllers/HelloAPI.java
+++ src/main/java/com/demoProject/Controllers/HelloAPI.java
@@ -1,19 +1,33 @@
 package com.demoProject.Controllers;
 
-import lombok.AllArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalTime;
 
 
 @Controller
-@RequestMapping("/demo")
-@AllArgsConstructor
 public class HelloAPI {
 
     @GetMapping("/index")
     public String indexPage(Model model) {
-        return "Hello";
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+        int minute = now.getMinute();
+        String gettin=null;
+
+        if(0 < hour  && hour >= 12 && 0 < minute && minute >= 59 ){
+            gettin= "Good morning";
+
+        }else if(12 < hour  && hour >= 16 && 0 < minute && minute >= 59){
+            gettin= "Good afternoon";
+        }else {
+            gettin= "Good evening";
+        }
+      model.addAttribute("message" , gettin);
+      return "Hello";
+
     }
 }
--- src/main/java/com/demoProject/DemoProjectApplication.java
+++ src/main/java/com/demoProject/DemoProjectApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoProjectApplication {
+    public static int add(int a, int b){
 
+		return a+b;
+	}
 	public static void main(String[] args) {
+		int result = add(3,4);
+        System.out.println("The sum is =" + result);
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
